4349859613173579ba2f298d0ee6a2bd2e58d247
Marcelo Ferracioli <marcelo@cammino.com.br>
2014-09-29 11:33:58 -0300

    implementa ecommerce avancado

--- Block/Ga.php
+++ Block/Ga.php
@@ -1,72 +1,74 @@
 <?php
 class Cammino_Googleanalytics_Block_Ga extends Mage_GoogleAnalytics_Block_Ga
 {
-    /**
-     * Render regular page tracking javascript code
-     * The custom "page name" may be set from layout or somewhere else. It must start from slash.
-     *
-     * @link http://code.google.com/apis/analytics/docs/gaJS/gaJSApiBasicConfiguration.html#_gat.GA_Tracker_._trackPageview
-     * @link http://code.google.com/apis/analytics/docs/gaJS/gaJSApi_gaq.html
-     * @param string $accountId
-     * @return string
-     */
+
     protected function _getPageTrackingCode($accountId)
     {
         $pageName   = trim($this->getPageName());
         $optPageURL = '';
+
         if ($pageName && preg_match('/^\/.*/i', $pageName)) {
             $optPageURL = ", '{$this->jsQuoteEscape($pageName)}'";
         }
+
+        $result[] = "ga('create','{$this->jsQuoteEscape($accountId)}', 'auto');";
+        $result[] = "ga('require', 'displayfeatures');";
+        $result[] = "ga('send', 'pageview'{$optPageURL});";
+        $result[] = "ga('require', 'ec');";
+
+        $result[] = $this->_getProductDetailsCode();
+        $result[] = $this->_getPurchaseCode();
     
-        
-        return "
-ga('create','{$this->jsQuoteEscape($accountId)}', 'auto');
-ga('require', 'displayfeatures');
-ga('send', 'pageview'{$optPageURL});
-";
+        return implode("\n", $result);
     }
 
-    /**
-     * Render information about specified orders and their items
-     *
-     * @link http://code.google.com/apis/analytics/docs/gaJS/gaJSApiEcommerce.html#_gat.GA_Tracker_._addTrans
-     * @return string
-     */
-    protected function _getOrdersTrackingCode()
+    protected function _getProductDetailsCode()
+    {
+        var_dump(Mage::registry('current_product'));
+
+        //$result[] = sprintf("ga('ec:addImpression', { 'id': 'P12345', 'name': 'Android Warhol T-Shirt', 'category': 'Apparel/T-Shirts', 'brand': 'Google', 'variant': 'Black', 'list': 'Related Products', 'position': 1, });");
+        //$result[] = sprintf("ga('ec:addProduct', { 'id': 'P12345', 'name': 'Android Warhol T-Shirt', 'category': 'Apparel', 'brand': 'Google', 'variant': 'Black', 'position': 1, });");
+        //$result[] = sprintf("ga('ec:setAction', 'detail');");
+
+        return implode("\n", $result);
+    }
+
+    protected function _getPurchaseCode()
     {
         $orderIds = $this->getOrderIds();
+
         if (empty($orderIds) || !is_array($orderIds)) {
             return;
         }
+
         $collection = Mage::getResourceModel('sales/order_collection')
             ->addFieldToFilter('entity_id', array('in' => $orderIds));
-        
-        $result = array("ga('require', 'ecommerce', 'ecommerce.js');");
 
         foreach ($collection as $order) {
+
             if ($order->getIsVirtual()) {
                 $address = $order->getBillingAddress();
             } else {
                 $address = $order->getShippingAddress();
             }
-            $result[] = sprintf("ga('ecommerce:addTransaction', {id: '%s', affiliation: '%s', revenue: '%s', tax: '%s', shipping: '%s' });",
+
+            foreach ($order->getAllVisibleItems() as $item) {
+                $result[] = sprintf("ga('ec:addProduct', { 'id': '%s', 'name': '%s', 'price': '%s', 'quantity': %s });",
+                    $this->jsQuoteEscape($item->getSku()),
+                    $this->jsQuoteEscape($item->getName()),
+                    number_format($item->getBasePrice(), 2, '.', ''),
+                    number_format($item->getQtyOrdered(), 0, '', '')
+                );
+            }
+
+            $result[] = sprintf("ga('ec:setAction', 'purchase', { 'id': '%s', 'affiliation': '%s', 'revenue': '%s', 'tax': '%s', 'shipping': '%s', 'coupon': '%s' });",
                 $order->getIncrementId(),
                 $this->jsQuoteEscape(Mage::app()->getStore()->getFrontendName()),
                 number_format($order->getBaseGrandTotal(), 2, '.', ''),
                 number_format($order->getBaseTaxAmount(), 2, '.', ''),
-                number_format($order->getBaseShippingAmount(), 2, '.', '')
+                number_format($order->getBaseShippingAmount(), 2, '.', ''),
+                $this->jsQuoteEscape($order->getCouponCode())
             );
-            foreach ($order->getAllVisibleItems() as $item) {
-                $result[] = sprintf("ga('ecommerce:addItem', {id: '%s', sku: '%s', name: '%s', category: '%s', price: '%s', quantity: '%s'});",
-                    $order->getIncrementId(),
-                    $this->jsQuoteEscape($item->getSku()),
-                    $this->jsQuoteEscape($item->getName()),
-                    null, // there is no "category" defined for the order item
-                    number_format($item->getBasePrice(), 2, '.', ''),
-                    number_format($item->getQtyOrdered(), 2, '.', '')
-                );
-            }
-            $result[] = "ga('ecommerce:send');";
         }
         return implode("\n", $result);
     }
